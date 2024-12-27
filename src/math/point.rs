0e6bc4aeccf2c6c51d65a7ec8b08ed614d8410e2
Lucas Prett Campagna <lucasprett120@gmail.com>
2024-12-26 23:01:28 -0300

    fixup! add dot and scalar products for point struct

--- src/math/point.rs
+++ src/math/point.rs
@@ -1,12 +1,12 @@
-use std::ops::{Add, AddAssign, Mul};
+use std::ops::{Add, AddAssign, Mul, Div};
 
 #[derive(Debug, Copy, Clone, Default, PartialEq)]
-pub struct Point<T: Add<Output = T> + Mul<Output = T> + Copy> {
+pub struct Point<T: Add<Output = T> + Mul<Output = T> + Div<Output = T> + Copy> {
     pub x: T,
     pub y: T,
 }
 
-impl<T: Add<Output = T> + Mul<Output = T> + Copy> Add for Point<T> {
+impl<T: Add<Output = T> + Mul<Output = T> + Div<Output = T> + Copy> Add for Point<T> {
     type Output = Self;
 
     fn add(self, other: Self) -> Self::Output {
@@ -16,14 +16,14 @@ impl<T: Add<Output = T> + Mul<Output = T> + Copy> Add for Point<T> {
         }
     }
 }
-impl<T: Add<Output = T> + Mul<Output = T> + Copy> Mul for Point<T> {
+impl<T: Add<Output = T> + Mul<Output = T> + Div<Output = T> + Copy> Mul for Point<T> {
     type Output = T;
     fn mul(self, other: Self) -> T {
         self.x * other.x + self.y * other.y
     }
 }
 
-impl<T: Add<Output = T> + Mul<Output = T> + Copy> Mul<T> for Point<T> {
+impl<T: Add<Output = T> + Mul<Output = T> + Div<Output = T> + Copy> Mul<T> for Point<T> {
     type Output = Self;
     fn mul(self, scalar: T) -> Self::Output {
         Self {
@@ -33,7 +33,17 @@ impl<T: Add<Output = T> + Mul<Output = T> + Copy> Mul<T> for Point<T> {
     }
 }
 
-impl<T: Add<Output = T> + Mul<Output = T> + Copy> AddAssign for Point<T> {
+impl<T: Add<Output = T> + Mul<Output = T> + Div<Output = T> + Copy> Div<T> for Point<T> {
+    type Output = Self;
+    fn div(self, scalar: T) -> Self::Output {
+        Self {
+            x: self.x / scalar,
+            y: self.y / scalar,
+        }
+    }
+}
+
+impl<T: Add<Output = T> + Mul<Output = T> + Div<Output = T> + Copy> AddAssign for Point<T> {
     fn add_assign(&mut self, other: Self) {
         *self = *self + other;
     }
